962223d5f2c2520ae9b8ce802e9a10f08bdbc11f
nguyentrungkienbh2007-cpu <nguyentrungkienbh2007@gmail.com>
2026-08-29 23:41:39 +0700

    Remove unused using directive and clean up interface

--- Code/GameCaroSocket/CaroGame.Application/UseCases/MatchMaking/IChallengeResponder.cs
+++ Code/GameCaroSocket/CaroGame.Application/UseCases/MatchMaking/IChallengeResponder.cs
@@ -1,9 +1,6 @@
-using CaroGame.Domain.Entities;
-
 namespace CaroGame.Application.UseCases.MatchMaking;
 
 public interface IChallengeResponder
 {
     Task<string?> RespondAsync(string challengerId, string opponentId, bool accept, CancellationToken cancellationToken = default);
 }
-
