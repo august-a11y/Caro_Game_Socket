796cfc294a3175908369e77490a8ea770612c539
Vỹ Lê <xuanvyhv572@gmail.com>
2026-09-05 07:33:03 +0700

    Remove unit tests

--- Code/GameCaroSocket/CaroGame.Application.Tests/UseCases/SessionUseCase/TestTimeProvider.cs
+++
@@ -1,8 +0,0 @@
-namespace CaroGame.Application.Tests.UseCases.SessionUseCase;
-
-internal sealed class TestTimeProvider(DateTimeOffset utcNow) : TimeProvider
-{
-    public DateTimeOffset UtcNow { get; set; } = utcNow;
-
-    public override DateTimeOffset GetUtcNow() => UtcNow;
-}
